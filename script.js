a17ac6ff3d43ac9814ace1a7e36f6f63b1b6a1be
zukomai <zukomail@local.com>
2022-11-26 15:18:32 -0700

    trying to figure out how to reset canvas if needed

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ diamondImg.src = 'assets/diamond-model.png';
 diamondImg.onload = function() {
     ctx.drawImage(diamondImg, 200, 150, 300, 350);
 }
+
+// Draw text
+const item1 = document.getElementById('item1');
+
+function imageText() {
+    if (item1.value == "") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    else if (item1.value != "") {
+    //item1.value = ''
+    ctx.fillStyle="black";
+    ctx.textAlign="center";
+    ctx.fillText(item1.value, 100, 100);
+    }
+
+}
